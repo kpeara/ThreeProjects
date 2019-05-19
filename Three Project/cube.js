ba675a81f2cb6faca8b9e5708fbcd1ac4fc506ee
kpeara <43894850+kpeara@users.noreply.github.com>
2019-05-19 09:54:46 -0400

    Update cube.js

--- Three Project/cube.js
+++ Three Project/cube.js
@@ -9,7 +9,7 @@ var camera = new THREE.PerspectiveCamera(
                 75, // field of view
                 width / height, // aspect ratio
                 0.1,
-                1000
+                10000
                 );
 
 var renderer = new THREE.WebGLRenderer();
@@ -42,38 +42,141 @@ var material = [
 ];
 
 // var material = new THREE.MeshFaceMaterial( cubeMaterial ); // material to color
-var cube = new THREE.Mesh(geometry, material); // applies material to geometry
 
-scene.add(cube); // adds cube to position 0,0,0
+var cubeArray = new Array();
+var inc = 1;
+var c = 1;
+var pos = 0
+for (var i = 0; i < 750; i++) {
+    var cube = new THREE.Mesh(geometry, material); // applies material to geometry
+    cube.position.x = pos + -inc * Math.random(); //inc + mx * Math.random();
+    cube.position.y = pos + inc * Math.random(); //inc + mx * Math.random();
+    cube.position.z = pos + -inc * Math.random() ; //inc + mx * Math.random();
+    scene.add(cube); // adds cube to position 0,0,0
+    inc = inc + c;
+    if (inc > 375) {
+        pos = 0;
+        inc = -1
+        c = -1
+    }
 
-camera.position.z = 5; // move camera back because it is current at (0,0,0) "inside the cube"
+    cubeArray.push(cube);
+}
+
+var sphereGeometry = new THREE.SphereGeometry(70,70,70);
+var sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xED51A1, side: THREE.DoubleSide, wireframe: true});
+var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+scene.add(sphere);
+
+
+camera.position.z = 1000; // move camera back because it is current at (0,0,0) "inside the cube"
+camera.position.x = -200; // move camera back because it is current at (0,0,0) "inside the cube"
+camera.position.y = 20; // move camera back because it is current at (0,0,0) "inside the cube"
+
+var colorCounter = 0;
+var movementCounter = 0;
 
 var incrementX = 0.01;
 var incrementY = -0.01;
 
-var counter = 0;
+var positionIncrement = 0.05 * (Math.random() ** 2);
+
 
 // double click to enable and disable roations
-window.addEventListener("dblclick", function() {
+window.addEventListener("click", function(evt) {
 
-    if (counter % 2 == 0) {
-        incrementX = 0;
-        incrementY = 0;
+    // var coreGeometry = new THREE.SphereGeometry(30,30,30);
+    // var coreMaterial = new THREE.MeshBasicMaterial({ color: 0xFF107A, side: THREE.DoubleSide});
+    // var core = new THREE.Mesh(coreGeometry, coreMaterial);
+
+    if (evt.detail == 3) {
+        if (colorCounter % 2 == 0) {
+            for (var i = 0; i < cubeArray.length; i++) {
+                for (var j = 0; j < material.length; j++) {
+                    
+        
+                    if (j != 2 && j != 4) {
+                        cubeArray[i].material[j].color.setHex(0xFFFFFF);
+                    }
+                    else {
+                        cubeArray[i].material[j].color.setHex(0x000000);
+                    }
+                    
+                    cubeArray[i].material[j].wireframe = false;
+                }
+            }
+        
+            sphere.material.color.setHex(0xFF475A);
+        }
+        else {
+            sphere.material.color.setHex(0xED51A1);
+
+            for (var i = 0; i < cubeArray.length; i++) {
+                cubeArray[i].material[0].color.setHex(0xBD7DE3);
+                
+                cubeArray[i].material[1].color.setHex(0x28F581);
+                cubeArray[i].material[1].wireframe = true;
+
+                cubeArray[i].material[2].color.setHex(0x7DD2E3);
+
+                cubeArray[i].material[3].color.setHex(0x28F581);
+                cubeArray[i].material[3].wireframe = true;
+
+                cubeArray[i].material[4].color.setHex(0xED51A1);
+
+                cubeArray[i].material[5].color.setHex(0x28F581);
+                cubeArray[i].material[5].wireframe = true;
+            }
+        }
+        colorCounter++;
     }
-    else {
-        incrementX = 0.01;
-        incrementY = -0.01;
+    else if (evt.detail == 2) { // double click
+        if (movementCounter % 2 == 0) {
+            incrementX = 0;
+            incrementY = 0;
+            positionIncrement = 0;
+        }
+        else {
+            incrementX = 0.01;
+            incrementY = -0.01;
+            positionIncrement = 0.05 * (Math.random() ** 2);
+        }
+        movementCounter++;
     }
-    counter++;
 });
 
 // a render loop, so cube is actually visible
 // calling other functions inside this is reccomended
+var g = 0;
 function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += incrementX;
-    cube.rotation.y += incrementY;
+    for (var i = 0; i < cubeArray.length; i++) {
+
+        cubeArray[i].rotation.x += incrementX;
+        cubeArray[i].rotation.y += incrementY;
+
+        if (cubeArray[i].position.x >= 0) {
+            cubeArray[i].position.x -= positionIncrement;
+        }
+        if (cubeArray[i].position.y >= 0) {
+            cubeArray[i].position.y -= positionIncrement;
+        }
+        if (cubeArray[i].position.z >= 0) {
+            cubeArray[i].position.z -= positionIncrement;
+        }
+
+        if (cubeArray[i].position.x <= 0) {
+            cubeArray[i].position.x += positionIncrement;
+        }
+        if (cubeArray[i].position.y <= 0) {
+            cubeArray[i].position.y += positionIncrement;
+        }
+        if (cubeArray[i].position.z <= 0) {
+            cubeArray[i].position.z += positionIncrement;
+        }
+    }
 
     renderer.render(scene, camera);
 }
